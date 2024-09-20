2e10f2c2a04e34159edde618f8f27507432cb5d4
menoo-o <123140589+menoo-o@users.noreply.github.com>
2024-09-20 11:28:10 +0500

    Add files via upload

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,23 @@
-const server = "listening"
+// server.js
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB//
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log(err));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
